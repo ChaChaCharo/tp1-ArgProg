3b86bbe0127862d05841bb51f39f77a4e33af4fc
Carolina Zapata <63057121+ChaChaCharo@users.noreply.github.com>
2023-11-09 00:01:44 -0300

    Completo el estilo y la config responsive.

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const planDescripcion = [
     
     'Una opción equilibrada que busca un compromiso entre la asequibilidad y la cobertura integral. Adecuado para aquellos que desean una protección más completa sin los costos elevados asociados con los planes premium, lo que lo convierte en una elección popular para familias y grupos numerosos.',
     
-    'La opción de nivel superior, que proporciona una cobertura amplia y beneficios adicionales, incluyendo: acceso a una red de proveedores de atención médica de primer nivel, cobertura dental y de visión, programas de bienestar y cobertura para viajes internacionales. El Plan Premium está diseñado para quienes buscan la máxima tranquilidad y el más alto nivel de atención.'
+    'La opción de nivel superior, que proporciona  beneficios adicionales, incluyendo: acceso a una red de proveedores de atención médica de primer nivel, cobertura dental y de visión, y cobertura para viajes internacionales. Diseñado para quienes buscan la máxima tranquilidad y el más alto nivel de atención.'
 ];
 
 
@@ -14,7 +14,7 @@ function mostrarTipoSeguro() {
     let indice = document.getElementById('tipoSeguro').value;
     if (indice != ''){
         document.getElementById("planSeleccionado").style.opacity = "1";
-        document.getElementById('planSeleccionado').innerHTML = '<img class="planImg" src="assets/' + planImgs[indice]+'"/><h3>Plan ' + planNombre[indice] + '</h3><p>'+ planDescripcion[indice] + '</p><big><b>Valor del Seguro: <strong>$' + planValor[indice] + '</strong></big>';    
+        document.getElementById('planSeleccionado').innerHTML = '<img class="planImg" src="assets/' + planImgs[indice]+'"/><p class="planNombre">Plan ' + planNombre[indice] + '<p><p class="valorPlan">Valor del Seguro: <strong>$' + planValor[indice] + '</p><p>'+ planDescripcion[indice] + '</p>';    
     }else{
         document.getElementById("planSeleccionado").style.opacity = "0";
         document.getElementById('planSeleccionado').innerHTML = '';
